d7803e83e13fb5b483a28109036060f089cde107
PMHStudio <pmhstudio.pmh@gmail.com>
2019-02-25 00:43:03 +0900

    프로토타입을 완성했습니다

--- kost.bat
+++ kost.bat
@@ -1,3 +1,21 @@
 @echo off
-node kost.js %*
+node -v
+if not "%ERRORLEVEL%" == "0" (
+    cls
+    echo Node.js is not installed
+    echo Open Installation site..
+    start https://nodejs.org
+    pause
+    exit
+)
+if not exist ./node_modules (
+    echo Downloading Modules...
+    npm install
+)
+echo * 이글이 보인다면 설치작업이 끝난것입니다! >./WorkStation/input.kost
+echo * 이제 아래에 한글스크립트를 작성, 저장하신후 >>./WorkStation/input.kost
+echo * 이 창을 닫으시면 됩니다! >>./WorkStation/input.kost
+start /wait notepad ./WorkStation/input.kost
+node kost.js input
+start ./output.txt
 echo on
